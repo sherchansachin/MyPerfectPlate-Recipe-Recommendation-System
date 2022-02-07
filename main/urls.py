9956854357d7c1044a7dc98be02d014eab57a630
Sachin Sherchan <sachinsherchan3@gmail.com>
2022-02-07 14:01:33 +0545

    view and urls update, can filter recipes and view recipe details

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
 
     path('', views.home, name="home"),
-    
-
+    # allrecipes
+    path('recipes/', views.recipes, name="recipes"),
+    path('recipes-details/<int:id>', views.recipe_details, name="recipeDetails"),
+    path('recipes/<slug:slug>', views.filter_category, name="filterCategory"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Recipes
 # Create your views here.
 
 
@@ -10,3 +10,55 @@ def home(request):
     return render(request, 'main/home.html')
 
 
+def recipes(request):
+    '''
+    this function based view displays all the recipes from db
+    '''
+    all_recipes = Recipes.objects.all()
+    recipe_count = Recipes.objects.count()
+    categories = Category.objects.all()
+
+
+    return render(request, "main/recipes.html", {"all_recipes": all_recipes,
+                                                "recipe_count": recipe_count,
+                                                "categories": categories    
+                                                })
+
+
+
+def filter_category(request, slug):
+    '''
+    this function based view filters through the categories of recipes
+    '''
+    individual_cat = get_object_or_404(Category, slug=slug)
+    recipes = Recipes.objects.filter(category=individual_cat)
+    count = recipes.count()
+    return render(request, 'main/category.html', {'individual_cat': individual_cat,
+                                                    'recipes':recipes,
+                                                    'count': count})
+
+
+
+def recipe_details(request, id):
+    '''
+    this function based view shows the detail information of a particular recipe
+    '''
+    details = get_object_or_404(Recipes, pk=id)
+
+    fav = bool
+
+    if details.favourites.filter(id=request.user.id).exists():
+        fav = True
+        
+    tags = details.tags.split('#')
+    ingredients = details.ingredients.split('#')
+    instructions = details.instructions.split('#')
+    categories = details.tags.split("#")
+    return render(request, 'main/recipe_details.html', {'details': details, 
+                                                'ingredients': ingredients,
+                                                'instructions': instructions,
+                                                'categories': categories,
+                                                'tags': tags,
+                                                'fav': fav,
+                                                })
+    
