78d411bcc989bbf2ae6e0e5ea4a7d3efbb5fc01e
Sachin Sherchan <sachinsherchan3@gmail.com>
2022-02-13 21:57:12 +0545

    form inputbox styling appended

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,21 +6,27 @@ from django.contrib.auth.forms import  UserCreationForm, PasswordResetForm, SetP
 class UserLoginForm(AuthenticationForm):
 
     username = forms.CharField(widget=forms.TextInput(
-        attrs={ 'id': 'login-username', 'autofocus': 'autofocus' }))
+        attrs={ 'id': 'login-username', 'autofocus': 'autofocus', 'class' : 'input-box' }))
     password = forms.CharField(widget=forms.PasswordInput(
-        attrs={'id': 'password'}))
+        attrs={'id': 'password', 'class' : 'input-box'}))
 
 class RegistrationForm(UserCreationForm):
     #additional field
     email = forms.EmailField(required=True)
 
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={ 'class' : 'input-box' }))
+
+    email = forms.CharField(widget=forms.TextInput(
+        attrs={ 'class' : 'input-box' }))
+
     password1 = forms.CharField(
         label='Password',
-        widget=forms.PasswordInput(attrs={'class': 'form-control mb-3',  'id': 'password'})
+        widget=forms.PasswordInput(attrs={'class': 'form-control mb-3',  'id': 'password', 'class' : 'input-box'})
     )
     password2 = forms.CharField(
         label='Password Confirmation',
-        widget=forms.PasswordInput(attrs={'class': 'form-control mb-3', 'id': 'password2'})
+        widget=forms.PasswordInput(attrs={'class': 'form-control mb-3', 'id': 'password2', 'class' : 'input-box'})
     )
 
     def clean_email(self):
@@ -38,7 +44,7 @@ class RegistrationForm(UserCreationForm):
 class PwdResetForm(PasswordResetForm):
 
     email = forms.EmailField(max_length=254, widget=forms.TextInput(
-        attrs={'class': 'form-control mb-3', 'placeholder': 'Email', 'id': 'form-email'}))
+        attrs={'class': 'form-control mb-3 input-box', 'placeholder': 'Email', 'id': 'form-email'}))
 
 # check to see if user email exists or not in the database
     def clean_email(self):
@@ -53,10 +59,10 @@ class PwdResetForm(PasswordResetForm):
 class PwdResetConfirmForm(SetPasswordForm):
     new_password1 = forms.CharField(
         label='New password', widget=forms.PasswordInput(
-            attrs={'class': 'form-control mb-3', 'placeholder': 'New Password', 'id': 'form-newpass'}))
+            attrs={'class': 'form-control mb-3 input-box' , 'placeholder': 'New Password', 'id': 'form-newpass'}))
     new_password2 = forms.CharField(
         label='Repeat password', widget=forms.PasswordInput(
-            attrs={'class': 'form-control mb-3', 'placeholder': 'New Password', 'id': 'form-new-pass2'}))
+            attrs={'class': 'form-control mb-3 input-box', 'placeholder': 'New Password', 'id': 'form-new-pass2'}))
 
 
 # user details edit form
@@ -64,15 +70,15 @@ class UserDetailForm(forms.ModelForm):
     
     first_name = forms.CharField(
         label='Firstname', min_length=4, max_length=50, widget=forms.TextInput(
-            attrs={'class': 'form-control mb-3', 'placeholder': 'Firstname', 'id': 'form-firstname'}))
+            attrs={'class': 'form-control mb-3 input-box', 'placeholder': 'Firstname', 'id': 'form-firstname'}))
 
     last_name = forms.CharField(
         label='Lastname', min_length=4, max_length=50, widget=forms.TextInput(
-            attrs={'class': 'form-control mb-3', 'placeholder': 'Lastname', 'id': 'form-lastname'}))
+            attrs={'class': 'form-control mb-3 input-box', 'placeholder': 'Lastname', 'id': 'form-lastname'}))
 
     email = forms.EmailField(
         max_length=200, widget=forms.TextInput(
-            attrs={'class': 'form-control mb-3', 'placeholder': 'Old Password', 'id': 'form-email'}))
+            attrs={'class': 'form-control mb-3 input-box', 'placeholder': 'Email', 'id': 'form-email'}))
 
     
 
