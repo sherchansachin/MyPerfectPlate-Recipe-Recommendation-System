f643a4fafb3747e16f7c5b20b3db9ed2e6af0f3e
Sachin Sherchan <sachinsherchan3@gmail.com>
2022-03-03 21:01:14 +0545

    meal plannerapp setup

--- planner/views.py
+++ planner/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required
 def mealplan(request):
-    pass
+    return render(request, 'planner/planner.html')
