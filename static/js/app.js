873715af4e5f985070e965824cded3a1a294e9b7
Sachin Sherchan <sachinsherchan3@gmail.com>
2022-04-23 15:53:17 +0545

    added remaining letters functionality for save notes

--- static/js/app.js
+++ static/js/app.js
@@ -21,3 +21,30 @@ if(togglePassword){
   });
 }
 
+
+// character remaining counter
+$(document).ready(function(){
+  var start = 0;
+  var limit = 400;
+
+  $("#message").keyup(function(){
+    start = this.value.length
+    if (start > limit) {
+      return false;
+    }
+    else if(start == 400){
+      $("#remaining").html("Remaining letters: " + (limit - start)).css('color', '#dc3545');
+      swal("oops! ", "Character limit exceeded!", "info");
+    }
+    else if(start > 384 ){
+      $("#remaining").html("Remaining letters: " + (limit - start)).css('color', '#dc3545');
+    }
+    else if(start < 400 ){
+      $("#remaining").html("Remaining letters: " + (limit - start)).css('color', '#198754');
+    }
+    else{
+      $("#remaining").html("Remaining letters: " + (limit)).css('color', '#198574');
+    }
+  })
+
+});
