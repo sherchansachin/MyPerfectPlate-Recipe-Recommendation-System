8b786a0b5ac9d5b92cc9b72f4d475102bc352b0b
Sachin Sherchan <sachinsherchan3@gmail.com>
2022-05-19 19:06:04 +0545

    collaborative recipe recommendation working, update on home page and views.py to communicate with Flask recommendation API

--- main/views.py
+++ main/views.py
@@ -1,24 +1,86 @@
+import re
 from django.shortcuts import render, get_object_or_404, redirect
 from datetime import datetime
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.contrib.auth.decorators import login_required
-from .models import Category, Recipes, Rating
+from .models import Category, Recipes, Rating, Note
 from .forms import ReviewForm
 from django.contrib import messages
 import requests
 import json
-
+from ast import literal_eval as make_tuple
 
 
 
 from django.http import HttpResponseRedirect
-# Create your views here.
+# Create your views here
 
 
 
 
 def home(request):
-    return render(request, 'main/home.html')
+    # get users id
+    user_id = request.user.id #get the user id
+    users = request.user # get the name of the user
+
+    # get the top 8 recently viewed recipes to show unauthenticated users
+    popular = Recipes.objects.all().order_by('-num_visits')[0:8]
+
+    # get ratings list
+    ratings = Rating.objects.all()
+    context = None
+
+    give_rating_msg = {
+        'message': "Opps! you must rate some recipes to get recommendations"
+    }
+
+    """
+        Check if user is logged in or not, if logged in communicate with flask to 
+        get recommedations, otherwise just show the popular recipes.
+    """
+    if request.user.is_authenticated:
+        # check if ratings were given by the user or not
+        ratings = Rating.objects.filter(user=users)
+        if len(ratings)!=0:
+            url = "http://127.0.0.1:5000/user_recommendation"
+            payload = {'user_id':user_id}
+            headers = {
+                'content-type': "multipart/form-data",
+                'cache-control': "no-cache",
+            }
+        
+            responses = requests.request("POST",url,data=payload)
+            print(responses) #got 200 response, OK
+
+            response = json.loads(responses.text)
+            print(response)
+
+            respnses_tuple = make_tuple(response)
+            # save the response in the obj_list variable
+            obj_list = []
+
+            for user_id in respnses_tuple:
+                try:
+                    recommended = Recipes.objects.get(id=user_id)
+                    obj_list.append(recommended)
+                except:
+                    pass
+            
+           
+        else:
+            return render(request, 'main/home.html', give_rating_msg )
+
+    else:
+        # get the top 8 recently viewed recipes to show unauthenticated users
+        popular_recipes = Recipes.objects.all().order_by('-num_visits')[0:8]
+       
+        return render(request, 'main/home.html', {'popular_recipes':popular_recipes,
+                                                    })
+
+    
+    # print(users)
+    return render(request, 'main/home.html',{'popular':popular,
+                                                'recommendated': obj_list})
 
 
 def recipes(request):
@@ -86,6 +148,7 @@ def recipe_details(request, id):
     obj_list = []
     recipe_name = details.title
     print(recipe_name)
+    
     if recipe_name:
         url = "http://127.0.0.1:5000/get_recommendation"
         payload = {'recipe_name':recipe_name}
@@ -112,7 +175,11 @@ def recipe_details(request, id):
 
     # capture the dateandtime for recently viewed recipes
     details.recently_viewed = datetime.now()
+
+    # capture the number of visits and save to db to get popular recipes
+    details.num_visits += 1
     details.save()
+
 
     # check if the particular recipe is saved by a user or not
     fav = bool
@@ -123,8 +190,7 @@ def recipe_details(request, id):
      # Get the reviews
     reviews = Rating.objects.filter(recipe_id=id)
 
-    # save notes
-    
+    note = Note.objects.filter(recipe_id=id, user_id=request.user.id)
         
     tags = details.tags.split('#')
     ingredients = details.ingredients.split('#')
@@ -137,9 +203,36 @@ def recipe_details(request, id):
                                                 'tags': tags,
                                                 'fav': fav,
                                                 'reviews': reviews,
+                                                'note': note,
                                                 'recommendated': obj_list
                                                 })
-    
+@login_required
+def save_notes(request, id):
+    """
+    this function saves the user's note written to the selected recipe
+    """
+    recipe = get_object_or_404(Recipes, pk=id)
+
+    if request.method == 'POST':
+        note = request.POST['note']
+        notes = Note()
+        notes.recipe = recipe
+        notes.user = request.user
+        notes.note = note
+        notes.save()
+
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
+
+@login_required
+def delete_note(request, id):
+    '''
+    this function removes/ deletes the note added for a particular recipes by the logged in users
+    '''
+    noted = Note.objects.get(recipe=id, user=request.user.id)
+    noted.delete()
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
 
 
 def submit_review(request, id):
